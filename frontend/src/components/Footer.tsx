22015e8574fc8ec83d1a95c0747e4283e6279ef4
thoraho <thoraho@hiof.no>
2024-09-25 18:49:30 +0200

    blabla nytt forsøk

--- frontend/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2024</p>
-    </footer>
-  )
-}
--- frontend/src/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
-  return (
-    <div id="logo">
-      <h1>WebApp 2024</h1>
-    </div>
-  )
-}
